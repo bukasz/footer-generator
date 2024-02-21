81625c9e35054eda9dd8a03098d411727e23a3d6
Lukasz Walaszczyk <lukasz.walaszczyk@creativestyle.pl>
2024-02-21 09:59:51 +0100

    Adjust footer styles, make phone optional

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="font-family: Arial, Helvetica, sans-serif; background: #fff">
+  <div style="font-family: Arial, Helvetica, sans-serif">
     <p style="font-size: 14px">
       Serdecznie pozdrawiam / Best regards / mit freundlichen Grüßen,
     </p>
@@ -7,7 +7,7 @@
       style="height: 40px; font-size: 13px; line-height: 15px; margin-top: 30px"
     >
       <img
-        src="https://lh3.googleusercontent.com/d/1JvCWAknaBbivlFfU-YEeloNhs58FpKv2"
+        src="https://lh3.googleusercontent.com/d/1oQ_11O2zFB0-PdFATqY69KkRkOXYtl0W"
         width="40"
         height="40"
         style="float: left; margin-right: 5px"
@@ -19,10 +19,11 @@
     </div>
 
     <div
+      v-if="phone"
       style="height: 40px; font-size: 13px; line-height: 15px; margin-top: 3px"
     >
       <img
-        src="https://lh3.googleusercontent.com/d/1WnKLN_Iy4a5VrvUKjxxRdw52FhwG3eH3"
+        src="https://lh3.googleusercontent.com/d/12tnBZ90fP6HTrKwmqcf8hr2GxCa9vt6I"
         width="40"
         height="40"
         style="float: left; margin-right: 5px"
@@ -45,7 +46,7 @@
       style="height: 40px; font-size: 13px; line-height: 15px; margin-top: 3px"
     >
       <img
-        src="https://lh3.googleusercontent.com/d/1Wd2SkBr5h_u691uqiLXUr0v_owA65HYD"
+        src="https://lh3.googleusercontent.com/d/1ahkKJ1uF-YcCtsWWZOSIsJRmAS7b__Us"
         width="40"
         height="40"
         style="float: left; margin-right: 5px"
@@ -64,69 +65,65 @@
       >
     </div>
 
-    <div
-      style="
-        width: 100%;
-        margin: 4px 0 0;
-        padding: 0;
-        color: #fff;
-        background: #232427;
-        font-family: Inter, Tahoma, sans-serif;
-        overflow: hidden;
-      "
-    >
+    <div style="margin: 4px 0 0; overflow: hidden">
       <img
-        src="https://lh3.googleusercontent.com/d/1UIT-C8CFI6i9a2-0a97t1UhrjcCezckU"
-        style="min-width: 500px; width: 100%; height: 39px; font-size: 0"
+        src="https://lh3.googleusercontent.com/d/1pcY5QzKtEVbCVu4gGqfyAqGfU-q6HKgE"
+        style="display: block; min-width: 603px; width: 100%; height: 39px"
       />
-      <img
-        src="https://lh3.googleusercontent.com/d/1rF9LUTv01VQqEMMDVWvPdD2-X2Fp_f5U"
+      <div
         style="
-          width: 130px;
-          height: 34px;
-          font-size: 0;
-          margin-top: 10px;
-          padding-left: 25px;
-        "
-      />
-      <p
-        style="
-          margin: 0;
-          padding: 20px 0 0 25px;
           color: #fff;
-          font-size: 12px;
-          line-height: 15px;
+          background: #232427;
+          font-family: Inter, Tahoma, sans-serif;
+          overflow: hidden;
         "
       >
-        creativestyle Polska Sp. z. o.o.<br />
-        ul. Zabłocie 25/L1,&#8203; 30-701 Kraków, Poland
-      </p>
-      <a
-        style="
-          display: block;
-          margin: 0;
-          padding: 20px 0 20px 25px;
-          color: #fff;
-          font-size: 12px;
-          line-height: 15px;
-        "
-        href="https://creativestyle.pl"
-      >
-        creativestyle.pl
-      </a>
-      <p
-        style="
-          margin: 0;
-          padding: 0 0 25px 25px;
-          color: #fff;
-          font-size: 12px;
-          line-height: 15px;
-        "
-      >
-        KRS 0000331561, Sąd Rejonowy dla Krakowa-<br />Śródmieścia, XI Wydział
-        Gospodarczy KRS,<br />
-        NIP PL6751416000, Kapitał zakładowy 50 000 zł<br />wpłacony w całości.
-      </p>
+        <img
+          src="https://lh3.googleusercontent.com/d/1IlGgHHbV8lIBoIKC5qstCAH6O0zaEWaU"
+          style="
+            width: auto;
+            height: 34px;
+            font-size: 0;
+            margin-top: 10px;
+            padding-left: 25px;
+          "
+        />
+        <p
+          style="
+            margin: 0;
+            padding: 20px 0 0 25px;
+            font-size: 12px;
+            line-height: 15px;
+          "
+        >
+          creativestyle Polska Sp. z. o.o.<br />
+          ul. Zabłocie 25/L1, 30-701 Kraków, Poland
+        </p>
+        <a
+          style="
+            display: block;
+            margin: 0;
+            padding: 20px 0 20px 25px;
+            color: #fff;
+            font-size: 12px;
+            line-height: 15px;
+          "
+          href="https://creativestyle.pl"
+        >
+          creativestyle.pl
+        </a>
+        <p
+          style="
+            margin: 0;
+            padding: 0 0 25px 25px;
+            font-size: 12px;
+            line-height: 15px;
+          "
+        >
+          KRS&nbsp;0000331561, Sąd&nbsp;Rejonowy&nbsp;dla&nbsp;Krakowa-<wbr />Śródmieścia, XI&nbsp;Wydział&nbsp;Gospodarczy&nbsp;KRS,<br />
+          NIP&nbsp;PL6751416000, Kapitał&nbsp;zakładowy&nbsp;50&nbsp;000&nbsp;zł wpłacony&nbsp;w&nbsp;całości.
+        </p>
+      </div>
     </div>
   </div>
 </template>
